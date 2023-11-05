a2534e0c4d9cf264d0277b9a9f225c0ca49e30fd
Michał Woźniak <m.wozniak60@student.uw.edu.pl>
2023-11-05 16:59:33 +0100

    Functions' descriptions changed to the Docstring format.

--- functions.py
+++ functions.py
@@ -5,10 +5,16 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import OneHotEncoder
 
 
-# This function builds a model with given input_dimension (number of words in dictionary),
-# output_dimension (number of classes), perceptrons list (where each number is the next layer),
-# activation function, and learning rate.
 def build_model(input_dimension=2, output_dimension=5, perceptrons=None, act='sigmoid', LR=0.2):
+    """
+    This function builds a model with given parameters.
+    :param input_dimension: number of words in dictionary
+    :param output_dimension: number of classes
+    :param perceptrons: list of perceptrons in each layer
+    :param act: activation function ('sigmoid' by default)
+    :param LR: Learning Rate (0.2 by default)
+    :return: compiled model
+    """
     if perceptrons is None:
         perceptrons = [8]
     # define model
@@ -30,8 +36,16 @@ def build_model(input_dimension=2, output_dimension=5, perceptrons=None, act='si
     return model
 
 
-# This functions trains the given model
 def fit_model(model, X, Y, epochs=300, validation_split=None):
+    """
+    Function that fits the given model to the given data.
+    :param model: tk.keras model to fit
+    :param X: matrix of input data
+    :param Y: vector of output labels
+    :param epochs: number of epochs
+    :param validation_split: size of the validation set (between 0-1)
+    :return: history of training and model's predictions
+    """
     # fit the model (fit: returns history of training)
     history = model.fit(X, Y, epochs=epochs, verbose=2, validation_split=validation_split)
 
@@ -41,8 +55,13 @@ def fit_model(model, X, Y, epochs=300, validation_split=None):
     return [history, y_hat]
 
 
-# This function generates plot of history of accuracy during training and/or validation.
 def plot_loss_accuracy(history, validation=False):
+    """
+    This function generates plot of history of accuracy during training and/or validation.
+    :param history: history of training
+    :param validation: boolean (False by default)
+    :return: nothing (draws a plot)
+    """
     loss = history['loss']
     acc = history['accuracy']
     legend = ['train']
@@ -70,11 +89,17 @@ def plot_loss_accuracy(history, validation=False):
     ax.grid(True)
     ax.set(xlabel='epochs', title='Accuracy')
     ax.legend(legend, loc='lower right')
+    plt.show()
     # plt.savefig("model1.svg")
 
 
-# This function splits the dataset into X and Y matrices and returns them, as well as the label encoder for Y.
 def split_xy(dataset):
+    """
+    This function splits the dataset into X and Y matrices and returns them,
+    as well as the label encoder for Y.
+    :param dataset: training set
+    :return: input data, output data, label encoder
+    """
     text = dataset['Text']
     lang = dataset['Language']
 
